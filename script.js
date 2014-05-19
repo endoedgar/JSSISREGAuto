02d77eedf284faa415a42964fdeb82a9964ec6b4
Edgar <edgar@central.saude.itapeva.sp.gov.br>
2014-05-19 14:01:25 -0300

    Adicionada mensagem de Falta justificativa e condição para aparecimento de botão

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ $(function(){
 	if(document.URL=='http://sisregiiisp.saude.gov.br/cgi-bin/autorizador' && $('textarea[name=justifDevolvido]').length) {
 		var cidade = $('table.table_listagem:nth-child(8) > tbody:nth-child(1) > tr:nth-child(10) > td:nth-child(2)').text();
 		var cid10 = $('input[name=cid_10]').val();
+		var procedimento = $('input[name=pa]').val();
 		var devEdit = $('textarea[name=justifDevolvido]');
 		var erros;
 		var condicoes = [
@@ -44,15 +45,23 @@ $(function(){
 		var botoes = [
 			{
 				botao: "CID incompatível com a justificativa",
+				condicao: true,
 				msg: "CID ("+cid10+") incompatível com a justificativa."
 			},
 			{
 				botao: "Nome incompleto do solicitante",
+				condicao: true,
 				msg: "Informar nome completo do solicitante."
 			},
 			{
 				botao: "Classificação de risco não justifica",
+				condicao: true,
 				msg: "Justificar a classificação de risco informada."
+			},
+			{
+				botao: "Falta justificativa",
+				condicao: true,
+				msg: "Conforme capacitação em 26/03/2014, todo o procedimento enviada para a regulação deve conter justificativa com o motivo da solicitação."
 			}
 		];
 		$('.erroBt').remove();
@@ -61,7 +70,9 @@ $(function(){
 		$('table.table_listagem:nth-child(1)').before('<article id="dev"><header>AUTO-Regulação</header><div><table><thead><tr><th>Item</th><th>Situação</th></tr></thead><tbody></tbody></table></div></article>');
 		$('div#devolvido').append('<center id="centD"></center>');
 		$.each(botoes, function() {
-			$('#centD').append('<input type="button"  class="erroBt" msg="'+this.msg+'" value="'+this.botao+'" />');
+			if(this.condicao) {
+				$('#centD').append('<input type="button"  class="erroBt" msg="'+this.msg+'" value="'+this.botao+'" />');
+			}
 		});
 		$('input.erroBt').click(function() {
 			devEdit.val(devEdit.val() + '- ' + $(this).attr('msg')+'\n');
